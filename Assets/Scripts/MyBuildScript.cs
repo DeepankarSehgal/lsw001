2addcb5e4837188b08ece31ab2fc28b888e9b84b
MSI\dheer <dheerajmandal78@gmail.com>
2024-08-30 01:27:26 +0530

    Build script

---
+++ Assets/Scripts/MyBuildScript.cs
@@ -0,0 +1,14 @@
+using UnityEditor;
+
+namespace MyCompany.Build
+{
+    public static class MyBuildScript
+    {
+        public static void BuildGame()
+        {
+            string[] scenes = { "Assets/Scenes/MainMenu.unity" };
+            string buildPath = "Builds/Windows/MyGame.exe";
+            BuildPipeline.BuildPlayer(scenes, buildPath, BuildTarget.StandaloneWindows64, BuildOptions.None);
+        }
+    }
+}
