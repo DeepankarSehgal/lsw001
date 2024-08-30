f9455a92051026f0e30b526abea077d57e500519
MSI\dheer <dheerajmandal78@gmail.com>
2024-08-30 13:37:51 +0530

    test 2 team

--- Assets/Scripts/MyBuildScript.cs
+++ Assets/Scripts/MyBuildScript.cs
@@ -7,7 +7,7 @@ namespace MyCompany.Build
         public static void BuildGame()
         {
             string[] scenes = { "Assets/Scenes/MainMenu.unity" };
-            string buildPath = "C:\\BuildAgent\\work\\17367024b001b829\\" + "Builds\\Windows\\MyGame.exe";
+            string buildPath = "Builds\\Windows\\MyGame.exe";
             BuildPipeline.BuildPlayer(scenes, buildPath, BuildTarget.StandaloneWindows64, BuildOptions.None);
         }
     }
