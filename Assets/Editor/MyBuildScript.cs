bcf2b9b349a08bacf01a653d73519d224d50fd52
MSI\dheer <dheerajmandal78@gmail.com>
2024-09-08 17:54:23 +0530

    ssss

--- Assets/Editor/MyBuildScript.cs
+++ Assets/Editor/MyBuildScript.cs
@@ -13,7 +13,7 @@ namespace MyCompany.Build
             string buildPath = "Builds/Windows.exe";
             BuildPipeline.BuildPlayer(scenes, buildPath, BuildTarget.StandaloneWindows64, BuildOptions.None);
         }
-        //Build  android message check. 1sssssss
+        //Build  android message check. 1ssssssssss
         [MenuItem("Build/Build Android")]
         public static void BuildGameAndroid()
         {
