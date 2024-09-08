a193d457cbb739d8dfc2b9fca3efe318f1852b01
MSI\dheer <dheerajmandal78@gmail.com>
2024-09-08 16:56:05 +0530

    build non message check

--- Assets/Editor/MyBuildScript.cs
+++ Assets/Editor/MyBuildScript.cs
@@ -13,7 +13,7 @@ namespace MyCompany.Build
             string buildPath = "Builds/Windows.exe";
             BuildPipeline.BuildPlayer(scenes, buildPath, BuildTarget.StandaloneWindows64, BuildOptions.None);
         }
-        //Build non android message check. 1
+        //Build nons android message check. 1
         [MenuItem("Build/Build Android")]
         public static void BuildGameAndroid()
         {
